a4431a3a76177e6ead2b90fd73f45b0e8f47dc05
Alan Berndt <alan@eatabrick.org>
2019-12-29 01:47:49 -0800

    Set up new song in test.

--- music/main.cc
+++ music/main.cc
@@ -4,15 +4,15 @@
 
 void dump_notes(std::vector<z2music::Note> notes) {
   for (z2music::Note n : notes) {
-    std::cerr << n.pitch_string();
+    std::cout << n.pitch_string();
     size_t left = n.length() / 4 - 4;
-    std::cerr << std::string(left, '.');
+    std::cout << std::string(left, '.');
   }
-  std::cerr << std::endl;
+  std::cout << std::endl;
 }
 
 void dump_song(const z2music::Song& song) {
-  std::cerr << "Song length: " << song.sequence_length() << " phrases" << std::endl;
+  std::cout << "Song length: " << song.sequence_length() << " phrases" << std::endl;
 
   for (size_t i = 0; i < song.sequence_length(); ++i) {
     const z2music::Pattern* p = song.at(i);
@@ -20,13 +20,13 @@ void dump_song(const z2music::Song& song) {
     dump_notes(p->notes(z2music::Pattern::Channel::Pulse2));
     dump_notes(p->notes(z2music::Pattern::Channel::Triangle));
     dump_notes(p->notes(z2music::Pattern::Channel::Noise));
-    std::cerr << std::endl;
+    std::cout << std::endl;
   }
 }
 
 int main(int argc, char** argv) {
   if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " z2_rom" << std::endl;
+    std::cout << "Usage: " << argv[0] << " z2_rom" << std::endl;
     return 1;
   }
 
@@ -34,6 +34,26 @@ int main(int argc, char** argv) {
 
   z2music::Rom rom(file);
   z2music::Song* battle = rom.song(z2music::Rom::SongTitle::BattleTheme);
+  dump_song(*battle);
+
+  battle->clear();
+
+  z2music::Pattern a;
+  z2music::Pattern b;
+
+  const z2music::Note CE(z2music::Note::Duration::Eighth, z2music::Note::Pitch::C4);
+  const z2music::Note CH(z2music::Note::Duration::Half, z2music::Note::Pitch::C4);
+  const z2music::Note DE(z2music::Note::Duration::Eighth, z2music::Note::Pitch::D4);
+  const z2music::Note DQ(z2music::Note::Duration::Quarter, z2music::Note::Pitch::D4);
+  const z2music::Note EQ(z2music::Note::Duration::Quarter, z2music::Note::Pitch::E4);
+
+  a.add_notes(z2music::Pattern::Channel::Pulse1, { EQ, DQ, CH });
+  b.add_notes(z2music::Pattern::Channel::Pulse1, { CE, CE, CE, CE, DE, DE, DE, DE });
+
+  battle->add_pattern(a);
+  battle->add_pattern(b);
+  battle->set_sequence({0, 0, 1, 0});
+
   dump_song(*battle);
 
   rom.save("/tmp/output.nes");
