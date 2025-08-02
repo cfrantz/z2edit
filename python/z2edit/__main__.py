cef3e36e0d5768f586cd0cf0ea46e135675a003f
Chris Frantz <frantzcj@gmail.com>
2025-08-01 20:57:43 -0700

    [z2edit] Update `main` to start either z2edit or emulator
    Signed-off-by: Chris Frantz <frantzcj@gmail.com>

--- python/z2edit/__main__.py
+++ python/z2edit/__main__.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
-from z2edit import app
+import sys
+from pathlib import Path
+
+progname = Path(sys.argv[0]).name
+
+if progname == "emulator":
+    from z2edit.emulator import app
+else:
+    from z2edit import app
 
 app.main()
